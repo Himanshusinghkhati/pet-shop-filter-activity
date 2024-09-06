33e90109ce3e271f020a89eba9168b3d65071c1d
Himanshu <119309525+Himanshusinghkhati@users.noreply.github.com>
2024-09-06 12:46:58 +0530

    git commit -m "pet shop"

---
+++ js/result.js
@@ -0,0 +1,63 @@
+let obj=[
+{id:1,pname:"Puppy",category:"dog",subcat:"puppy",pimage:"./image/puppy1.webp",url:"https://simple.wikipedia.org/wiki/Pet#:~:text=A%20pet%20is%20a%20domesticated,to%20take%20care%20of%20them."},
+{id:2,pname:"Cats",category:"cat",subcat:"cat",pimage:"./image/cat1.webp",url:"https://simple.wikipedia.org/wiki/Pet#:~:text=A%20pet%20is%20a%20domesticated,to%20take%20care%20of%20them."},
+{id:3,pname:"Parrot",category:"parrot",subcat:"parrot",pimage:"./image/parrot1.webp",url:"https://simple.wikipedia.org/wiki/Pet#:~:text=A%20pet%20is%20a%20domesticated,to%20take%20care%20of%20them."},
+{id:4,pname:"Rabbit",category:"rabbit",subcat:"rabbit",pimage:"./image/rabbit1.webp",url:"https://simple.wikipedia.org/wiki/Pet#:~:text=A%20pet%20is%20a%20domesticated,to%20take%20care%20of%20them."},
+{id:5,pname:"Labrador",category:"dog",subcat:"labrador",pimage:"./image/lebra1.webp",url:"https://simple.wikipedia.org/wiki/Pet#:~:text=A%20pet%20is%20a%20domesticated,to%20take%20care%20of%20them."},
+{id:6,pname:"Parrot",category:"parrot",subcat:"parrot",pimage:"./image/parrot2.webp",url:"https://simple.wikipedia.org/wiki/Pet#:~:text=A%20pet%20is%20a%20domesticated,to%20take%20care%20of%20them."},
+{id:7,pname:"Cats",category:"cat",subcat:"cat",pimage:"./image/cat2.webp",url:"https://simple.wikipedia.org/wiki/Pet#:~:text=A%20pet%20is%20a%20domesticated,to%20take%20care%20of%20them."},
+{id:8,pname:"Puppy",category:"dog",subcat:"puppy",pimage:"./image/puppy2.webp",url:"https://simple.wikipedia.org/wiki/Pet#:~:text=A%20pet%20is%20a%20domesticated,to%20take%20care%20of%20them."},
+{id:9,pname:"German Shepherd",category:"dog",subcat:"german shepherd",pimage:"./image/german1.webp",url:"https://simple.wikipedia.org/wiki/Pet#:~:text=A%20pet%20is%20a%20domesticated,to%20take%20care%20of%20them."},
+{id:10,pname:"Rabbit",category:"rabbit",subcat:"rabbit",pimage:"./image/rabbit2.webp",url:"https://simple.wikipedia.org/wiki/Pet#:~:text=A%20pet%20is%20a%20domesticated,to%20take%20care%20of%20them."},
+{id:11,pname:"Labrador",category:"dog",subcat:"labrador",pimage:"./image/lebra2.webp",url:"https://simple.wikipedia.org/wiki/Pet#:~:text=A%20pet%20is%20a%20domesticated,to%20take%20care%20of%20them."},
+{id:12,pname:"Cats",category:"cat",subcat:"cat",pimage:"./image/cat3.webp",url:"https://simple.wikipedia.org/wiki/Pet#:~:text=A%20pet%20is%20a%20domesticated,to%20take%20care%20of%20them."},
+{id:13,pname:"German Shepherd",category:"dog",subcat:"german shepherd",pimage:"./image/german2.webp",url:"https://simple.wikipedia.org/wiki/Pet#:~:text=A%20pet%20is%20a%20domesticated,to%20take%20care%20of%20them."},
+{id:14,pname:"Rabbit",category:"rabbit",subcat:"rabbit",pimage:"./image/rabbit3.webp",url:"https://simple.wikipedia.org/wiki/Pet#:~:text=A%20pet%20is%20a%20domesticated,to%20take%20care%20of%20them."},
+{id:15,pname:"Labrador",category:"dog",subcat:"labrador",pimage:"./image/lebra3.webp",url:"https://simple.wikipedia.org/wiki/Pet#:~:text=A%20pet%20is%20a%20domesticated,to%20take%20care%20of%20them."},
+{id:16,pname:"Parrot",category:"parrot",subcat:"parrot",pimage:"./image/parrot3.webp",url:"https://simple.wikipedia.org/wiki/Pet#:~:text=A%20pet%20is%20a%20domesticated,to%20take%20care%20of%20them."},
+{id:17,pname:"Puppy",category:"dog",subcat:"puppy",pimage:"./image/puppy3.webp",url:"https://simple.wikipedia.org/wiki/Pet#:~:text=A%20pet%20is%20a%20domesticated,to%20take%20care%20of%20them."},
+{id:18,pname:"Rabbit",category:"rabbit",subcat:"rabbit",pimage:"./image/rabbit4.webp",url:"https://simple.wikipedia.org/wiki/Pet#:~:text=A%20pet%20is%20a%20domesticated,to%20take%20care%20of%20them."},
+{id:19,pname:"Cats",category:"cat",subcat:"cat",pimage:"./image/cat4.webp",url:"https://simple.wikipedia.org/wiki/Pet#:~:text=A%20pet%20is%20a%20domesticated,to%20take%20care%20of%20them."},
+{id:20,pname:"Labrador",category:"dog",subcat:"labrador",pimage:"./image/lebra4.webp",url:"https://simple.wikipedia.org/wiki/Pet#:~:text=A%20pet%20is%20a%20domesticated,to%20take%20care%20of%20them."},
+{id:21,pname:"Parrot",category:"parrot",subcat:"parrot",pimage:"./image/parrot4.webp",url:"https://simple.wikipedia.org/wiki/Pet#:~:text=A%20pet%20is%20a%20domesticated,to%20take%20care%20of%20them."},
+{id:22,pname:"Puppy",category:"dog",subcat:"puppy",pimage:"./image/puppy4.webp",url:"https://simple.wikipedia.org/wiki/Pet#:~:text=A%20pet%20is%20a%20domesticated,to%20take%20care%20of%20them."},
+{id:23,pname:"Rabbit",category:"rabbit",subcat:"rabbit",pimage:"./image/rabbit5.webp",url:"https://simple.wikipedia.org/wiki/Pet#:~:text=A%20pet%20is%20a%20domesticated,to%20take%20care%20of%20them."},
+{id:24,pname:"German Shepherd",category:"dog",subcat:"german shepherd",pimage:"./image/german3.webp",url:"https://simple.wikipedia.org/wiki/Pet#:~:text=A%20pet%20is%20a%20domesticated,to%20take%20care%20of%20them."},
+{id:25,pname:"Cats",category:"cat",subcat:"cat",pimage:"./image/cat5.webp",url:"https://simple.wikipedia.org/wiki/Pet#:~:text=A%20pet%20is%20a%20domesticated,to%20take%20care%20of%20them."},
+{id:26,pname:"Rabbit",category:"rabbit",subcat:"rabbit",pimage:"./image/rabbit6.webp",url:"https://simple.wikipedia.org/wiki/Pet#:~:text=A%20pet%20is%20a%20domesticated,to%20take%20care%20of%20them."},
+{id:27,pname:"Cats",category:"cat",subcat:"cat",pimage:"./image/cat6.webp",url:"https://simple.wikipedia.org/wiki/Pet#:~:text=A%20pet%20is%20a%20domesticated,to%20take%20care%20of%20them."},
+{id:28,pname:"German Shepherd",category:"dog",subcat:"german shepherd",pimage:"./image/german4.webp",url:"https://simple.wikipedia.org/wiki/Pet#:~:text=A%20pet%20is%20a%20domesticated,to%20take%20care%20of%20them."},
+]
+
+function displayAllProducts(products){
+   
+    let newarr=products.map((item)=>{
+        return(
+          `
+          <div class="product">
+          <img class="p-image" src='${item.pimage}' loading="lazy">
+          <div class="content">
+          <h4><u>${item.pname}</u></h4>
+          <a href='${item.url}' target='_blank'>
+                <button><b>Know More</b></button>
+            </a>
+          </div>
+          </div>
+          `
+        )
+      })
+      document.getElementById("myrow").innerHTML=newarr.join('');
+   }
+
+   // Filter function to display products based on category or subcategory
+function filterdata(filterValue) {
+  let filteredProducts = obj.filter((item) => {
+      return item.category === filterValue || item.subcat === filterValue;
+  });
+  displayAllProducts(filteredProducts);
+}
+
+// Display all products initially
+displayAllProducts(obj);
+
+
